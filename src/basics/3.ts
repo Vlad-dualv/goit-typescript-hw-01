99e4238fdeb6fab0f980c68335cd3efc88f75860
Vlad <vladswork95@gmail.com>
2025-03-04 15:11:44 +0000

    Basics - done

---
+++ src/basics/3.ts
@@ -0,0 +1,12 @@
+let stringOrNumber: string | number;
+stringOrNumber = "string";
+stringOrNumber = 22;
+
+console.log(stringOrNumber);
+
+type Option = "enable" | "disable";
+let option: Option;
+option = "enable";
+option = "disable";
+
+console.log(option);
